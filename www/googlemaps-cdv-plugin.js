59c404718e25b64f8d30374af5096464eae6f22f
Benjamin Thomas <ben@bentomas.com>
2015-04-09 11:10:25 -0700

    remove debugging line

--- www/googlemaps-cdv-plugin.js
+++ www/googlemaps-cdv-plugin.js
@@ -954,7 +954,7 @@ App.prototype.addPolyline = function(polylineOptions, callback) {
   polylineOptions.visible     = polylineOptions.visible === undefined ? true : polylineOptions.visible;
   polylineOptions.zIndex      = polylineOptions.zIndex || 4;
   polylineOptions.geodesic    = polylineOptions.geodesic || false;
-  console.log("color = " + polylineOptions.color.join(", "));
+  //console.log("color = " + polylineOptions.color.join(", "));
 
   cordova.exec(function(result) {
     var polyline = new Polyline(self, result.id, polylineOptions);
