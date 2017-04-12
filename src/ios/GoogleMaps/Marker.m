83639d9f5f5aab80f4be85220502e6bd9b89f54e
Benjamin Thomas <ben@bentomas.com>
2017-04-12 13:25:27 -0700

    Revert previous change

--- src/ios/GoogleMaps/Marker.m
+++ src/ios/GoogleMaps/Marker.m
@@ -85,11 +85,6 @@
         [iconProperty setObject:[rgbColor parsePluginColor] forKey:@"iconColor"];
     }
 
-    if ([json valueForKey:@"anchor"]) {
-      NSArray *points = [json valueForKey:@"anchor"];
-      [iconProperty setObject:points forKey:@"anchor"];
-    }
-
     // Visible property
     if ([[json valueForKey:@"visible"] boolValue] == true) {
         iconProperty[@"visible"] = @YES;
